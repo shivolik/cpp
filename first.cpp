23cc7a4bda418b7a1b1cb22a5dac8e2ca04e41d5
shivolik <thakuraaarav11@gmail.com>
2026-04-17 10:13:27 +0530

    second-commit

--- first.cpp
+++ first.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
+void sum(int a, int b){
+    cout<<"The sum is: "<<a+b<<endl;
+}
+
 int main(){
     cout<<"Hello World"<<endl;
+    sum(5, 10);
     return 0;
 }
