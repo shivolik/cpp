02313576484c44ed5febaa1b4be15213e342e2a3
shivolik <thakuraaarav11@gmail.com>
2026-04-17 10:24:02 +0530

    thirdcommit

--- first.cpp
+++ first.cpp
@@ -5,8 +5,13 @@ void sum(int a, int b){
     cout<<"The sum is: "<<a+b<<endl;
 }
 
+void subtract(int a, int b){
+    cout<<"The difference is: "<<a-b<<endl;
+}
+
 int main(){
     cout<<"Hello World"<<endl;
     sum(5, 10);
+    subtract(10, 5);
     return 0;
 }
